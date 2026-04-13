8e6e29f6d89316913347d30b105438042dee1e9f
Olesia <borutsao@gmail.com>
2026-04-13 18:39:32 +0300

    setup inventory API service and routing for admin panel

---
+++ src/services/inventoryApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:3000' });
+
+// ОСЬ ТУТ має бути слово export
+export const inventoryApi = {
+  getAll: () => API.get('/inventory'),
+  getOne: (id) => API.get(`/inventory/${id}`),
+  create: (formData) => API.post('/register', formData),
+  updateText: (id, data) => API.put(`/inventory/${id}`, data),
+  updatePhoto: (id, formData) => API.put(`/inventory/${id}/photo`, formData),
+  delete: (id) => API.delete(`/inventory/${id}`)
+};
